238d036daff91a4426df6d1c60bb64d2dc705d33
folkse7 <folkse7@users.noreply.github.com>
2023-03-29 22:51:39 +0400

    adding features section

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,12 +3,14 @@ import { HeroTitle } from './main';
 import {GetInTouchSimple } from './contact';
 import { OurStory } from './story';
 import { FooterSocial } from './footer';
+import { FeaturesTitle } from './features';
 
 export default function Homepage() {
   return (
       <>
       <HeaderAction links={[{ link: "Wordbanks.js", label: "Word Banks" }, { link: "https://google.com", label: "Exam Guides" }, { link: "https://google.com", label: "Past Papers" }]}> </HeaderAction>
       <HeroTitle></HeroTitle>
+      <FeaturesTitle></FeaturesTitle>
       <OurStory></OurStory>
       <GetInTouchSimple></GetInTouchSimple>
       <FooterSocial></FooterSocial>
