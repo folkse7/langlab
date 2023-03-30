4d088d23e679c707b3eebecd171b538992fb92e2
folkse7 <folkse7@users.noreply.github.com>
2023-03-30 04:12:17 +0400

    starting carousel for past paper

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { MantineProvider, Text } from '@mantine/core';
 import Homepage from './components/homepage';
 import WordBanks from './components/wordbanks';
 import Guides from './components/guides';
+import PastPaper from './components/pastpaper';
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
         {/* <Homepage></Homepage> */}
         {/* <WordBanks></WordBanks> */}
-        <Guides></Guides>
+        {/* <Guides></Guides> */}
+        <PastPaper></PastPaper>
     </MantineProvider>
   );
 }
--- src/components/pastpaper.js
+++ src/components/pastpaper.js
@@ -1,15 +1,11 @@
-import { FooterSocial } from './footer';
-import { ImageBackground } from './gheader';
-import { GuideInputs } from './guidemenu';
+import { CardsCarousel } from './carousel';
 import { HeaderAction } from './headeraction';
 
-export default function Guides() {
+export default function PastPaper() {
   return (
       <>
       <HeaderAction links={[{ link: "Wordbanks.js", label: "Word Banks" }, { link: "https://google.com", label: "Exam Guides" }, { link: "https://google.com", label: "Past Papers" }]}> </HeaderAction>
-      <ImageBackground></ImageBackground>
-      <GuideInputs></GuideInputs>
-      <FooterSocial></FooterSocial>
+      <CardsCarousel></CardsCarousel>
       </>
   );
 }
