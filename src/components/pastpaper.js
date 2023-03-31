49aa0b41ee0d5b2ce8fa0af941672f29cb1ea2a2
folkse7 <folkse7@users.noreply.github.com>
2023-03-31 16:14:20 +0400

    past papers

--- src/components/pastpaper.js
+++ src/components/pastpaper.js
@@ -1,4 +1,5 @@
 import { CardsCarousel } from './carousel';
+import { FooterSocial } from './footer';
 import { HeaderAction } from './headeraction';
 import { TitlePaper } from './pheader';
 import { PaperMenu } from './ppmenu';
@@ -9,7 +10,8 @@ export default function PastPaper() {
 <HeaderAction links={[{ link: "wordbanks", label: "Word Banks" }, { link: "guides", label: "Exam Guides" }, { link: "pastpapers", label: "Past Papers" }]}> </HeaderAction>      
 <TitlePaper></TitlePaper>
 <PaperMenu></PaperMenu>
-<CardsCarousel></CardsCarousel>
+{/* <CardsCarousel></CardsCarousel> */}
+<FooterSocial></FooterSocial>
       </>
   );
 }
